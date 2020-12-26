744c2c8fea23ea93aa901b497c2ce0d966010d00
Chan <ahs0432@gmail.com>
2020-12-26 17:48:22 +0900

    test

---
+++ prime.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main(){
+	int num = 0;
+	int prime = 0;
+	scanf("%d", &num);
+	
+	for (int i=2;i <= num/2; i++){
+		if (num%i==0){
+			prime=1;
+			break;
+		}
+	}
+
+	if (prime==0){
+		printf("%d is prime number\n", num);
+	} else {
+		printf("%d is not prime number\n", num);
+	}
+
+	return 0;
+}
