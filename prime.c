1ac0b0c7cebf45cc0d1fce9a84ee876f151a9f3b
Chan <ahs0432@gmail.com>
2020-12-26 18:35:53 +0900

    20201226

--- prime.c
+++ prime.c
@@ -2,16 +2,19 @@
 
 void main(){
 	int num = 0;
-	printf("소수를 확인할 정수를 입력해주세요.: ");
 
-	if(scanf("%d", &num)==0){
-		printf("정수만 입력이 가능합니다!\n");
-		return;
-	}
+	while (1) {
+		printf("소수를 확인할 정수를 입력해주세요.: ");
+		
+		if(scanf("%d", &num)==0){
+			printf("정수만 입력이 가능합니다!\n");
+		}else if(num<=0){
+			printf("1 이상의 수를 입력해주세요!\n");
+		}else{
+			break;
+		}
 
-	if(num<=0){
-		printf("1 이상의 수를 입력해주세요!\n");
-		return;
+		getchar();
 	}
 	
 	for (int i=2;i <= num/2; i++){
