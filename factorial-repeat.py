304642a7c6095fdee13f90992e61c8beab4b1418
Chan <ahs0432@gmail.com>
2021-01-02 12:25:04 +0900

    Rep factorial_repeat.py

--- factorial-repeat.py
+++ factorial-repeat.py
@@ -1,9 +1,7 @@
 def factorial_re(n):
-    sum = 1
-    if n==0:
-        return sum
-    sum = factorial_re(n-1)
-    return sum * n
+    if n==1:
+        return 1
+    return factorial_re(n-1) * n
 
 fval = int(input("입력해주세요. > "))
 print(factorial_re(fval))
