3255c2ecfde3b89f9073ee3041e3c0562fb3991d
Chan <ahs0432@gmail.com>
2021-01-02 09:31:37 +0900

    20210102 2

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,19 +5,18 @@ public class PrimeNumber {
         Scanner scv = new Scanner(System.in);
         int n = scv.nextInt();
 
-        // 1과 자기 자신 외에는 나누어 떨어지지 않는 수, 소수.
         int count = 0;
 
         for(int k =1; k<=n; k++){
-            if(n%k==0) { // 1부터 1씩 증가하며 입력 받을 수가 나누어 떨어지는 횟수를 Count
+            if(n%k==0) { 
                 count ++;
             }
         }
 
         if(count == 2){
-            System.out.println(n + " is prime number");
+            System.out.println(n + " is prime number!");
         } else {
-            System.out.println(n + " is not prime number");
+            System.out.println(n + " is not prime number!");
         }
     }
 }
