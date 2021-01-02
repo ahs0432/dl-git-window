a546655b382caa44a4f3f566e55ef02d6a2c3fae
Chan <ahs0432@gmail.com>
2021-01-02 09:49:45 +0900

    v6 count delete

--- PrimeNumber.java
+++ PrimeNumber.java
@@ -5,16 +5,19 @@ public class PrimeNumber {
         Scanner scv = new Scanner(System.in);
         int n = scv.nextInt();
 
-        int count = 0;
+        //int count = 0;
+        boolean isPrime = true;
 
         for(int k = 2; k<n; k++){
             if(n%k==0) {
-                count ++;
+                // count++; // count = count + 1;
+                isPrime = false;
                 break;
             }
         }
 
-        if(count == 0){
+        // if(count == 0) {
+        if(isPrime){
             System.out.println(n + " is prime number!");
         } else {
             System.out.println(n + " is not prime number!");
